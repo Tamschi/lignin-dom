bd809d9f6393311cb8088e2fd4585f5f3ddffa9c
Tamme Schichler <tamme@schichler.dev>
2020-10-02 18:08:08 +0200

    Added meta data and adjusted dependencies a little

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# lignin-dom Changelog
+
+## 0.0.1
+
+2020-10-02
+
+Initial unstable release
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+#![doc(html_root_url = "https://docs.rs/lignin-dom/0.0.1")]
+#![warn(clippy::pedantic)]
+
 pub use lignin;
+
+#[cfg(doctest)]
+pub mod readme {
+	doc_comment::doctest!("../README.md");
+}
 
 mod closure_map;
 pub mod diff;
