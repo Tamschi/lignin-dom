7e528411dc4dab5c8dcb6ea32e91be3438c511ec
Rust Format Action <rust-format-action@schichler.dev>
2021-03-17 13:30:17 +0000

    Automatic formatting commit (cargo +nightly fmt)

--- tests/web_allocator_.rs
+++ tests/web_allocator_.rs
@@ -1,12 +1,12 @@
-use lignin_dom::load::{self, };
+use lignin_dom::load::{self};
 
 pub struct Allocator;
-impl load::Allocator<'static> for Allocator{
-    fn allocate<T>(&self, instance: T) -> &'static T {
-        Box::leak(Box::new(instance))
-    }
+impl load::Allocator<'static> for Allocator {
+	fn allocate<T>(&self, instance: T) -> &'static T {
+		Box::leak(Box::new(instance))
+	}
 
-    fn allocate_slice<T>(&self, iter: &mut dyn ExactSizeIterator<Item = T>) -> &'static [T] {
-        iter.collect::<Vec<_>>().leak()
-    }
+	fn allocate_slice<T>(&self, iter: &mut dyn ExactSizeIterator<Item = T>) -> &'static [T] {
+		iter.collect::<Vec<_>>().leak()
+	}
 }
