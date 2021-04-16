3d38ead57fbe171f7d38b385358147cbfb17707b
Tamme Schichler <tamme@schichler.dev>
2021-04-16 14:15:43 +0200

    Added (currently failing) event test

---
+++ tests/web_event.rs
@@ -0,0 +1,64 @@
+use lignin::{web::Event, CallbackRef, CallbackRegistration, DomRef, Element, ElementCreationOptions, EventBinding, EventBindingOptions, Materialize, Node, ReorderableFragment, ThreadBound};
+use lignin_dom::{diff::DomDiffer, load::Allocator as _};
+use std::cell::RefCell;
+use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
+use web_sys::{window, HtmlBodyElement, HtmlElement};
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+static mut LOG_INITIALIZED: bool = false;
+
+#[wasm_bindgen_test]
+fn click() {
+	unsafe {
+		if !LOG_INITIALIZED {
+			//TODO: Fail on Warnig or Error.
+			tracing_wasm::set_as_global_default();
+			LOG_INITIALIZED = true;
+		}
+	}
+
+	let body = window().unwrap().document().unwrap().body().unwrap().dyn_into::<HtmlBodyElement>().unwrap();
+
+	let mut differ = DomDiffer::new_for_element_child_nodes(body.into());
+
+	let attributes = [lignin::Attribute { name: "id", value: "test-button" }];
+
+	let click_count = Box::pin(RefCell::new(0));
+
+	let callback = CallbackRegistration::<_, fn(Event)>::new(click_count.as_ref(), |click_count, event| {
+		let event = event.materialize();
+		let event: &JsValue = event.as_ref();
+		event.dyn_ref::<web_sys::Event>().expect("Expected Event but reeived something else.");
+
+		*unsafe { click_count.as_ref() }.unwrap().borrow_mut() += 1;
+	});
+
+	let bindings = [EventBinding {
+		name: "click",
+		callback: callback.to_ref_thread_bound(),
+		options: EventBindingOptions::new(),
+	}];
+
+	let element = Element {
+		name: "BUTTON",
+		creation_options: ElementCreationOptions::new(),
+		attributes: &attributes,
+		content: Node::Multi(&[]),
+		event_bindings: &bindings,
+	};
+
+	let vdom = Node::HtmlElement { element: &element, dom_binding: None };
+
+	assert_eq!(*click_count.borrow(), 0);
+	differ.update_child_nodes(&[], &[vdom], 1);
+	assert_eq!(*click_count.borrow(), 0);
+
+	let button: HtmlElement = window().unwrap().document().unwrap().get_element_by_id("test-button").unwrap().dyn_into().unwrap();
+	button.click();
+
+	assert_eq!(*click_count.borrow(), 1);
+	differ.update_child_nodes(&[vdom], &[], 1);
+	assert_eq!(*click_count.borrow(), 1);
+}
