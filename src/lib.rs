d15ec81781b75c42d44d2fb1af65f242aadf0318
Rust Format Action <rust-format-action@schichler.dev>
2021-06-06 23:55:48 +0000

    Automatic formatting commit (cargo +nightly fmt)

--- src/lib.rs
+++ src/lib.rs
@@ -127,7 +127,7 @@
 //! ## Starting a DOM Tree from Scratch
 //!
 //! When rendering into an empty element, you can simply start with `&[]` as initial `vdom_a` parameter.
-//! 
+//!
 //! This also works for non-empty elements where you'd like to insert your app at the beginning of the child node list.
 //!
 //! > I recommend not doing this except for systems where you can control every aspect of execution and startup UX is of no concern.
